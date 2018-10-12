b6b768bfd01f03a5256c2db4b84f9d7a42149de2
wolf-zchen <zhelinchen91@gmail.com>
2018-10-12 12:45:02 -0500

    First commit

---
+++ ros/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/zchen61/CarND-Capstone/ros/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/zchen61/bin:/home/zchen61/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/mnt/c/Program Files (x86)/Intel/iCLS Client:/mnt/c/Program Files/Intel/iCLS Client:/mnt/c/Windows/System32:/mnt/c/Windows:/mnt/c/Windows/System32/wbem:/mnt/c/Windows/System32/WindowsPowerShell/v1.0:/mnt/c/Program Files/Intel/WiFi/bin:/mnt/c/Program Files/Common Files/Intel/WirelessCommon:/mnt/c/Program Files (x86)/Intel/Intel(R) Management Engine Components/DAL:/mnt/c/Program Files/Intel/Intel(R) Management Engine Components/DAL:/mnt/c/Program Files (x86)/Intel/Intel(R) Management Engine Components/IPT:/mnt/c/Program Files/Intel/Intel(R) Management Engine Components/IPT:/mnt/c/Program Files (x86)/NVIDIA Corporation/PhysX/Common:/mnt/c/Program Files/MiKTeX 2.9/miktex/bin/x64:/mnt/c/Windows/System32:/mnt/c/Windows:/mnt/c/Windows/System32/wbem:/mnt/c/Windows/System32/WindowsPowerShell/v1.0:/mnt/c/Program Files/Git/cmd:/mnt/c/Program Files (x86)/Microsoft SQL Server/100/Tools/Binn:/mnt/c/Program Files/Microsoft SQL Server/100/Tools/Binn:/mnt/c/Program Files/Microsoft SQL Server/100/DTS/Binn:/mnt/c/Program Files (x86)/Intel/Compiler/Fortran/9.1/em64t/bin:/mnt/c/Windows/System32/OpenSSH:/mnt/c/Users/Z.Chen/AppData/Local/Continuum/Anaconda3:/mnt/c/Users/Z.Chen/AppData/Local/Continuum/Anaconda3/Scripts:/mnt/c/Users/Z.Chen/AppData/Local/Continuum/Anaconda3/Library/bin:/mnt/c/Users/Z.Chen/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/Z.Chen/AppData/Local/atom/bin:/mnt/c/Users/Z.Chen/AppData/Local/Microsoft/WindowsApps:/snap/bin"
+export PWD="/home/zchen61/CarND-Capstone/ros/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/zchen61/CarND-Capstone/ros/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/zchen61/CarND-Capstone/ros/src:$ROS_PACKAGE_PATH"
